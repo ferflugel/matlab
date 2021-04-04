1a4b7f79df6621b206c3cb56bf035ff1685c2efd
Fernando Flugel Assad <63256562+ferflugel@users.noreply.github.com>
2021-04-04 12:32:46 -0300

    Update solver.m

--- solver.m
+++ solver.m
@@ -7,6 +7,10 @@
 % the otput will always be in the phasor
 % form (magnitude / angle in degrees).  
 
+% If you need to transform a phasor to algebraic form, use:
+% m = (magnitude); x = (angle in degrees);
+% alg = m * (cos(x * (pi / 180)) + 1i * sin(x * (pi / 180)))
+
 syms v1 v2 v3 v4 v5;
 eqns = [
 (1 + 1i)*v1 + (1 + 1i)*v2 + (1 + 1i)*v3 + (0 + 0i)*v4 + (0 + 0i)*v5 == 0
