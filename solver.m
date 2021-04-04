219dc194c0d4f2c0358bcc8088d70c086326b20a
Fernando Flugel Assad <63256562+ferflugel@users.noreply.github.com>
2021-04-03 23:08:55 -0300

    Update solver.m

--- solver.m
+++ solver.m
@@ -5,30 +5,35 @@
 
 % IMPORTANT: 
 % the otput will always be in the phasor
-% form (magnitude / angle). If you want to
-% get the algebraic form, just type 
-% S.v(number) in the command window. 
+% form (magnitude / angle in degrees).  
 
 syms v1 v2 v3 v4 v5;
 eqns = [
-(1 + 1i)*v1 + (1 + 1i)*v2 + (1 + 1i)*v3 == 0
-(1 + 1i)*v1 + (1 + 1i)*v2 + (1 + 1i)*v3 == 0
-(1 + 1i)*v1 + (1 + 1i)*v2 + (1 + 1i)*v3 == 0
+(1 + 1i)*v1 + (1 + 1i)*v2 + (1 + 1i)*v3 + (0 + 0i)*v4 + (0 + 0i)*v5 == 0
+(1 + 1i)*v1 + (1 + 1i)*v2 + (1 + 1i)*v3 + (0 + 0i)*v4 + (0 + 0i)*v5 == 0
+(1 + 1i)*v1 + (1 + 1i)*v2 + (1 + 1i)*v3 + (0 + 0i)*v4 + (0 + 0i)*v5 == 0
+%(1 + 1i)*v1 + (1 + 1i)*v2 + (1 + 1i)*v3 + (0 + 0i)*v4 + (0 + 0i)*v5 == 0
+%(1 + 1i)*v1 + (1 + 1i)*v2 + (1 + 1i)*v3 + (0 + 0i)*v4 + (0 + 0i)*v5 == 0
 ];
 
 S = solve(eqns, [v1, v2, v3, v4, v5]);
 
 [angle, magnitude] = cart2pol(real(S.v1), imag(S.v1));
+v1 = S.v1
 v1_ph = [double(magnitude), double((180/pi) * angle)]
 
 [angle, magnitude] = cart2pol(real(S.v2), imag(S.v2));
+v2 = S.v2
 v2_ph = [double(magnitude), double((180/pi) * angle)]
 
 [angle, magnitude] = cart2pol(real(S.v3), imag(S.v3));
+v3 = S.v3
 v3_ph = [double(magnitude), double((180/pi) * angle)]
 
 [angle, magnitude] = cart2pol(real(S.v4), imag(S.v4));
+v4 = S.v4
 v4_ph = [double(magnitude), double((180/pi) * angle)]
 
 [angle, magnitude] = cart2pol(real(S.v5), imag(S.v5));
+v5 = S.v5
 v5_ph = [double(magnitude), double((180/pi) * angle)]
